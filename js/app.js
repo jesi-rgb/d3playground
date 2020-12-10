2aa4ad3bfbfc9cf5e39cc5105b71555225afdb7f
Jesús Enrique Rascón <blograso@gmail.com>
2020-12-10 16:09:00 +0100

    managed to generate axes, weird behaviour

--- js/app.js
+++ js/app.js
@@ -1,39 +1,59 @@
 var data = _.range(0, 20)
 
-var width = 500;
-var height = 500;
-var barHeight = 50;
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-var xScale = d3.scaleLinear()
-               .domain([0, d3.max(data)])
-               .range([0, width]);
-
-var yScale = d3.scaleLinear()
-                .domain([0, data.length])
-                .range([height, barHeight]);
-
-var colorScale = d3.scaleLinear()
-                    .domain([d3.min(data), d3.max(data)])
-                    .range(["pink", "red", "blue"]);
 
 var canvas = d3.select("#chart")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height);
+				.append("svg")
+				.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+				.append("g")
+				.attr("transform",
+					"translate(" + margin.left + "," + margin.top + ")");
+
+					
+					
+					
+d3.csv("../data/prueba.csv", d3.autoType, function(data){
+	var groups = _.countBy(data, "genre");
 
 
-var bars = canvas.selectAll("rect")
-             .data(data)
-             .enter()
-                .append("rect")
-                .attr("width", (d) => {return xScale(d);})
-                .attr("height", (d) => {return yScale(d);})
-                .attr("fill", (d) => {return colorScale(d)})
-                .attr("y", (d, i) => {return i*(barHeight+5);});
+	var x = d3.scaleBand()
+		.range([ 0, width ])
+		.domain(_.map(data, d => {return d.genre}))
+		.padding(0.2);
+	
 
-// d3.csv("../data/SpotifyFeatures.csv", d3.autoType, function(data){
-//     console.log(data)
-// })
+	canvas.append("g")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x))
+			.selectAll("text")
+			.attr("transform", "translate(-10,0)rotate(-45)")
+			.style("text-anchor", "end");
+
+	var y = d3.scaleLinear()
+			.domain([0, d3.max(_.map(groups, d => {return d}))])
+			.range([ height, 0]);
+
+	canvas.append("g")
+		.call(d3.axisLeft(y));
+
+
+	
+	console.log(groups);
+
+	canvas.selectAll("mybar")
+		.data(groups)
+		.enter()
+		.append("rect")
+			.attr("x", (d) => { return d.genre; })
+			.attr("y", (d) => { return d; })
+			.attr("width", x.bandwidth())
+			.attr("height", function(d) { return height - y(d); })
+			.attr("fill", "#69b3a2");
+});
 
 
 
